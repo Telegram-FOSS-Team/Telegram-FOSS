30e5196ebf80b3c559c0c8d56ea347f4ee84ab41
DrKLO <drklo.2kb@gmail.com>
2017-04-06 09:47:48 +0200

    [REVIEW] ui/Cells

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/GraySectionCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/GraySectionCell.java
@@ -3,7 +3,7 @@
  * It is licensed under GNU GPL v. 2 or later.
  * You should have received a copy of the license in this archive (see LICENSE).
  *
- * Copyright Nikolai Kudashov, 2013-2016.
+ * Copyright Nikolai Kudashov, 2013-2017.
  */
 
 package org.telegram.ui.Cells;
@@ -16,20 +16,22 @@ import android.widget.TextView;
 
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.LocaleController;
+import org.telegram.ui.ActionBar.Theme;
 import org.telegram.ui.Components.LayoutHelper;
 
-public class GreySectionCell extends FrameLayout {
+public class GraySectionCell extends FrameLayout {
+
     private TextView textView;
 
-    public GreySectionCell(Context context) {
+    public GraySectionCell(Context context) {
         super(context);
 
-        setBackgroundColor(0xfff2f2f2);
+        setBackgroundColor(Theme.getColor(Theme.key_graySection));
 
         textView = new TextView(getContext());
         textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 13);
         textView.setTypeface(AndroidUtilities.getTypeface("fonts/rmedium.ttf"));
-        textView.setTextColor(0xff8a8a8a);
+        textView.setTextColor(Theme.getColor(Theme.key_windowBackgroundWhiteGrayText2));
         textView.setGravity((LocaleController.isRTL ? Gravity.RIGHT : Gravity.LEFT) | Gravity.CENTER_VERTICAL);
         addView(textView, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT, (LocaleController.isRTL ? Gravity.RIGHT : Gravity.LEFT) | Gravity.TOP, 16, 0, 16, 0));
     }
--- TMessagesProj/src/main/java/org/telegram/ui/Cells/TextColorThemeCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/TextColorThemeCell.java
@@ -8,8 +8,6 @@
 
 package org.telegram.ui.Cells;
 
-import android.animation.Animator;
-import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
@@ -20,12 +18,9 @@ import android.widget.TextView;
 
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.LocaleController;
-import org.telegram.ui.ActionBar.Theme;
 import org.telegram.ui.Components.LayoutHelper;
 
-import java.util.ArrayList;
-
-public class TextColorCell extends FrameLayout {
+public class TextColorThemeCell extends FrameLayout {
 
     private TextView textView;
     private boolean needDivider;
@@ -34,10 +29,7 @@ public class TextColorCell extends FrameLayout {
 
     private static Paint colorPaint;
 
-    public final static int colors[] = new int[] {0xfff04444, 0xffff8e01, 0xffffce1f, 0xff79d660, 0xff40edf6, 0xff46beff, 0xffd274f9, 0xffff4f96, 0xffbbbbbb};
-    public final static int colorsToSave[] = new int[] {0xffff0000, 0xffff8e01, 0xffffce1f, 0xff00ff00, 0xff40edf6, 0xff0000ff, 0xffd274f9, 0xffff4f96, 0xffffffff};
-
-    public TextColorCell(Context context) {
+    public TextColorThemeCell(Context context) {
         super(context);
 
         if (colorPaint == null) {
@@ -45,13 +37,14 @@ public class TextColorCell extends FrameLayout {
         }
 
         textView = new TextView(context);
-        textView.setTextColor(Theme.getColor(Theme.key_windowBackgroundWhiteBlackText));
+        textView.setTextColor(0xff212121);
         textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 16);
         textView.setLines(1);
         textView.setMaxLines(1);
         textView.setSingleLine(true);
         textView.setGravity((LocaleController.isRTL ? Gravity.RIGHT : Gravity.LEFT) | Gravity.CENTER_VERTICAL);
-        addView(textView, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT, (LocaleController.isRTL ? Gravity.RIGHT : Gravity.LEFT) | Gravity.TOP, 17, 0, 17, 0));
+        textView.setPadding(0, 0, 0, AndroidUtilities.dp(3));
+        addView(textView, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT, (LocaleController.isRTL ? Gravity.RIGHT : Gravity.LEFT) | Gravity.TOP, (LocaleController.isRTL ? 17 : 17 + 36), 0, (LocaleController.isRTL ? 17 + 36 : 17), 0));
     }
 
     @Override
@@ -70,33 +63,19 @@ public class TextColorCell extends FrameLayout {
         super.onMeasure(MeasureSpec.makeMeasureSpec(MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(AndroidUtilities.dp(48) + (needDivider ? 1 : 0), MeasureSpec.EXACTLY));
     }
 
-    public void setTextAndColor(String text, int color, boolean divider) {
+    public void setTextAndColor(String text, int color) {
         textView.setText(text);
-        needDivider = divider;
         currentColor = color;
         setWillNotDraw(!needDivider && currentColor == 0);
         invalidate();
     }
 
-    public void setEnabled(boolean value, ArrayList<Animator> animators) {
-        if (animators != null) {
-            animators.add(ObjectAnimator.ofFloat(textView, "alpha", value ? 1.0f : 0.5f));
-            animators.add(ObjectAnimator.ofFloat(this, "alpha", value ? 1.0f : 0.5f));
-        } else {
-            textView.setAlpha(value ? 1.0f : 0.5f);
-            setAlpha(value ? 1.0f : 0.5f);
-        }
-    }
-
     @Override
     protected void onDraw(Canvas canvas) {
-        if (needDivider) {
-            canvas.drawLine(getPaddingLeft(), getHeight() - 1, getWidth() - getPaddingRight(), getHeight() - 1, Theme.dividerPaint);
-        }
         if (currentColor != 0) {
             colorPaint.setColor(currentColor);
             colorPaint.setAlpha((int) (255 * alpha));
-            canvas.drawCircle(LocaleController.isRTL ? AndroidUtilities.dp(29) : getMeasuredWidth() - AndroidUtilities.dp(29), getMeasuredHeight() / 2, AndroidUtilities.dp(10), colorPaint);
+            canvas.drawCircle(!LocaleController.isRTL ? AndroidUtilities.dp(28) : getMeasuredWidth() - AndroidUtilities.dp(28 + 20), getMeasuredHeight() / 2, AndroidUtilities.dp(10), colorPaint);
         }
     }
 }
