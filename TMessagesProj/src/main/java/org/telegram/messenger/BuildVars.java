1fb77463339cfc93ecc82f13999735291e5a54e1
thermatk <thermatk@thermatk.com>
2017-07-10 17:20:35 +0300

    [TF] kill bing image search

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -15,7 +15,6 @@ public class BuildVars {
     public static String BUILD_VERSION_STRING = "4.1";
     public static int APP_ID = 12834; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
     public static String APP_HASH = "c84d9229db1d6be95c067b02b126352c"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
-    public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
     public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
     public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
     public static String FOURSQUARE_API_VERSION = "20150326";
