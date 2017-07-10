7b014a84dc85433bda480b55de359e80b2140f95
thermatk <thermatk@thermatk.com>
2017-07-10 17:02:04 +0300

    [TF] API keys

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -13,8 +13,8 @@ public class BuildVars {
     public static boolean DEBUG_PRIVATE_VERSION = false;
     public static int BUILD_VERSION = 1043;
     public static String BUILD_VERSION_STRING = "4.2";
-    public static int APP_ID = 0; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static int APP_ID = 12834; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "c84d9229db1d6be95c067b02b126352c"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
     public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
     public static String HOCKEY_APP_HASH_DEBUG = "your-hockeyapp-api-key-here";
     public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
