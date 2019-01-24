1b3af0a710cbda8c316fff2febafd505ef9d105a
thermatk <thermatk@thermatk.com>
2019-01-24 19:25:40 +0100

    [TF][KILL] Hockey analytics

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -22,8 +22,6 @@ public class BuildVars {
     public static String BUILD_VERSION_STRING = "5.9.0";
     public static int APP_ID = BuildConfig.APP_ID; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
     public static String APP_HASH = BuildConfig.APP_HASH; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
-    public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
-    public static String HOCKEY_APP_HASH_DEBUG = "your-hockeyapp-api-key-here";
     public static String SMS_HASH = ""; //https://developers.google.com/identity/sms-retriever/overview
     public static String PLAYSTORE_APP_URL = "";
 
