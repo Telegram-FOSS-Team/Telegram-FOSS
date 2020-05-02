0f4131dfec00496416ae5408eb001bf703462b42
thermatk <thermatk@thermatk.com>
2020-05-02 11:42:02 +0200

    [TF][BRAND] Send invite link to F-Droid

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -30,10 +30,7 @@ public class BuildVars {
     public static String APP_HASH = BuildConfig.APP_HASH; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
 
     // SafetyNet key for Google Identity SDK, set it to empty to disable
-    public static String SAFETYNET_KEY = "AIzaSyDqt8P-7F7CPCseMkOiVRgb1LY8RN1bvH8";
-    public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
-    public static String HUAWEI_STORE_URL = "https://appgallery.huawei.com/app/C101184875";
-    public static String GOOGLE_AUTH_CLIENT_ID = "760348033671-81kmi3pi84p11ub8hp9a1funsv0rn2p9.apps.googleusercontent.com";
+    public static String PLAYSTORE_APP_URL = "https://f-droid.org/en/packages/org.telegram.messenger/";
 
     public static String HUAWEI_APP_ID = "101184875";
 
