a8691a7b121b087afd7d7ec43c935cf6e9f3d6d5
thermatk <thermatk@thermatk.com>
2021-01-02 22:33:11 +0100

    [TF][MAPS] support geo: intents
    This patch adds the ability for Telegram to parse locations from send
    intents containing a geo:<lat>,<lon>,<zoom> string.
    
    This is done by various map applications, including the popular OSMAnd
    and Orux maps. This patch enables the user to share the position of a
    POI or destination with Telegram and have it send as a location.

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -33,6 +33,9 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
 import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.location.Location;
 import android.location.LocationManager;
 import android.media.AudioManager;
 import android.net.Uri;
@@ -192,6 +195,8 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
     private static final String EXTRA_ACTION_TOKEN = "actions.fulfillment.extra.ACTION_TOKEN";
 
     private boolean finished;
+    final private Pattern locationRegex = Pattern.compile("geo: ?(-?\\d+\\.\\d+),(-?\\d+\\.\\d+)(,|\\?z=)(-?\\d+)");
+    private Location sendingLocation;
     private String videoPath;
     private String sendingText;
     private ArrayList<SendMessagesHelper.SendingMediaInfo> photoPathsArray;
@@ -1580,6 +1585,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
         photoPathsArray = null;
         videoPath = null;
         sendingText = null;
+        sendingLocation = null;
         documentsPathsArray = null;
         documentsOriginalPathsArray = null;
         documentsMimeType = null;
@@ -1656,7 +1662,28 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
                         String subject = intent.getStringExtra(Intent.EXTRA_SUBJECT);
 
                         if (!TextUtils.isEmpty(text)) {
-                            if ((text.startsWith("http://") || text.startsWith("https://")) && !TextUtils.isEmpty(subject)) {
+                                Matcher m = locationRegex.matcher(text);
+                                if (m.find()) {
+                                    String lines[] = text.split("\\n");
+                                    String venueTitle = null;
+                                    String venueAddress = null;
+                                    if (lines[0].equals("My Position")){
+                                        // Use normal GeoPoint message (user position)
+                                    }
+                                    else if(!lines[0].contains("geo:")){
+                                        venueTitle = lines[0];
+                                        if(!lines[1].contains("geo:")){
+                                            venueAddress = lines[1];
+                                        }
+                                    }
+                                    sendingLocation = new Location("");
+                                    sendingLocation.setLatitude(Double.parseDouble(m.group(1)));
+                                    sendingLocation.setLongitude(Double.parseDouble(m.group(2)));
+                                    Bundle bundle = new Bundle();
+                                    bundle.putCharSequence("venueTitle", venueTitle);
+                                    bundle.putCharSequence("venueAddress", venueAddress);
+                                    sendingLocation.setExtras(bundle);
+                                } else if ((text.startsWith("http://") || text.startsWith("https://")) && !TextUtils.isEmpty(subject)) {
                                 text = subject + "\n" + text;
                             }
                             sendingText = text;
@@ -1738,7 +1765,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
                                     }
                                 }
                             }
-                        } else if (sendingText == null) {
+                        } else if (sendingText == null && sendingLocation == null) {
                             error = true;
                         }
                     }
@@ -2698,7 +2725,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
                     }
                 });
                 pushOpened = false;
-            } else if (videoPath != null || photoPathsArray != null || sendingText != null || documentsPathsArray != null || contactsToSend != null || documentsUrisArray != null) {
+            } else if (videoPath != null || photoPathsArray != null || sendingText != null || sendingLocation != null || documentsPathsArray != null || contactsToSend != null || documentsUrisArray != null) {
                 if (!AndroidUtilities.isTablet()) {
                     NotificationCenter.getInstance(intentAccount[0]).postNotificationName(NotificationCenter.closeChats);
                 }
@@ -4951,6 +4978,10 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
                         }
                         SendMessagesHelper.prepareSendingDocuments(accountInstance, documentsPathsArray, documentsOriginalPathsArray, documentsUrisArray, captionToSend, documentsMimeType, did, null, null, null, null, notify, 0);
                     }
+	                if (sendingLocation != null) {
+	                    SendMessagesHelper.prepareSendingLocation(accountInstance, sendingLocation, did);
+	                    sendingText = null;
+	                }
                     if (sendingText != null) {
                         SendMessagesHelper.prepareSendingText(accountInstance, sendingText, did, true, 0);
                     }
@@ -4973,6 +5004,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
         photoPathsArray = null;
         videoPath = null;
         sendingText = null;
+        sendingLocation = null;
         documentsPathsArray = null;
         documentsOriginalPathsArray = null;
         contactsToSend = null;
