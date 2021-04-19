3a1940bfcf3987f1351991aceeb6c2bfd0d89a21
thermatk <thermatk@thermatk.com>
2021-04-19 11:01:49 +0200

    [TF][FEATURE] no restrictions

--- TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
@@ -132,7 +132,7 @@ public class ApplicationLoader extends Application {
     }
 
     public static boolean isStandaloneBuild() {
-        return applicationLoaderInstance.isStandalone();
+        return true;
     }
 
     protected boolean isHuaweiBuild() {
@@ -140,7 +140,7 @@ public class ApplicationLoader extends Application {
     }
 
     protected boolean isStandalone() {
-        return false;
+        return true;
     }
 
     public static File getFilesDirFixed() {
