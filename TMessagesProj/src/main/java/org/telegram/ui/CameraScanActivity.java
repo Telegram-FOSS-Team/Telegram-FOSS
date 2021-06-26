a0d14145faea9bf96c6b69bbf20882027667d7c8
nekohasekai <sekai@neko.services>
2021-06-26 17:10:09 +0200

    [TF][FIX] the inverse color QR code scanned without vision

--- TMessagesProj/src/main/java/org/telegram/ui/CameraScanActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/CameraScanActivity.java
@@ -56,6 +56,7 @@ import androidx.dynamicanimation.animation.SpringForce;
 
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.LuminanceSource;
+import com.google.zxing.NotFoundException;
 import com.google.zxing.PlanarYUVLuminanceSource;
 import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.Result;
@@ -1339,7 +1340,15 @@ public class CameraScanActivity extends BaseFragment {
                     height = size.getHeight();
                 }
 
-                Result result = qrReader.decode(new BinaryBitmap(new GlobalHistogramBinarizer(source)));
+                Result result = null;
+                try {
+                    result = qrReader.decode(new BinaryBitmap(new GlobalHistogramBinarizer(source)));
+                } catch (NotFoundException e) {
+                    try {
+                        result = qrReader.decode(new BinaryBitmap(new GlobalHistogramBinarizer(source.invert())));
+                    } catch (NotFoundException ignore) {
+                    }
+                }
                 if (result == null) {
                     onNoQrFound();
                     return null;
