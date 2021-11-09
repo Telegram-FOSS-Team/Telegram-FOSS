f482d73158fc42b0260c2427695d0f2a0f2abde4
23rd <23rd@vivaldi.net>
2021-11-09 22:04:41 +0300

    Added ability to set target platforms for FFmpeg and BoringSSL.
    Usage: ./build_ffmpeg_clang.sh arm arm64.

--- TMessagesProj/jni/patch_ffmpeg.sh
+++ TMessagesProj/jni/patch_ffmpeg.sh
@@ -5,6 +5,10 @@ set -e
 patch -d ffmpeg -p1 < patches/ffmpeg/0001-compilation-magic.patch
 patch -d ffmpeg -p1 < patches/ffmpeg/0002-compilation-magic-2.patch
 
+function cp {
+	install -D $@
+}
+
 cp ffmpeg/libavformat/dv.h ffmpeg/build/arm64-v8a/include/libavformat/dv.h
 cp ffmpeg/libavformat/isom.h ffmpeg/build/arm64-v8a/include/libavformat/isom.h
 cp ffmpeg/libavformat/dv.h ffmpeg/build/armeabi-v7a/include/libavformat/dv.h
