43f5fe50fcab2d99ce4c7135fd9acd03f0f1f209
thermatk <thermatk@thermatk.com>
2022-02-08 22:53:35 +0100

    [TF][KILL] Google Translate without MLKit

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -121,7 +121,6 @@ import org.telegram.messenger.FileLog;
 import org.telegram.messenger.ForwardingMessagesParams;
 import org.telegram.messenger.ImageLocation;
 import org.telegram.messenger.ImageReceiver;
-import org.telegram.messenger.LanguageDetector;
 import org.telegram.messenger.LocaleController;
 import org.telegram.messenger.MediaController;
 import org.telegram.messenger.MediaDataController;
@@ -21700,7 +21699,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     };
                     TLRPC.InputPeer inputPeer = getMessagesController().getInputPeer(dialog_id);
                     int messageId = selectedObject.messageOwner.id;
-                    if (LanguageDetector.hasSupport()) {
+                    /*if (LanguageDetector.hasSupport()) {
                         final String[] fromLang = { null };
                         cell.setVisibility(View.GONE);
                         waitForLangDetection.set(true);
@@ -21740,7 +21739,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                                 onLangDetectionDone.getAndSet(null).run();
                             }
                         }, 250);
-                    } else {
+                    } else {*/
                         cell.setOnClickListener(e -> {
                             if (selectedObject == null || i >= options.size() || getParentActivity() == null) {
                                 return;
@@ -21749,7 +21748,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                             alert.showDim(false);
                             closeMenu(false);
                         });
-                    }
+                    //}
                 }
             }
 
