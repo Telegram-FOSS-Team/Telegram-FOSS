3be55759df6c636ffcb4f2168c3c46cea982431a
thermatk <thermatk@thermatk.com>
2022-07-19 21:15:29 +0200

    [TF][KILL] Google Translate without MLKit

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -121,7 +121,6 @@ import org.telegram.messenger.FileLog;
 import org.telegram.messenger.ForwardingMessagesParams;
 import org.telegram.messenger.ImageLocation;
 import org.telegram.messenger.ImageReceiver;
-import org.telegram.messenger.LanguageDetector;
 import org.telegram.messenger.LocaleController;
 import org.telegram.messenger.MediaController;
 import org.telegram.messenger.MediaDataController;
@@ -22019,7 +22018,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                         };
                         TLRPC.InputPeer inputPeer = getMessagesController().getInputPeer(dialog_id);
                         int messageId = selectedObject.messageOwner.id;
-                        if (LanguageDetector.hasSupport()) {
+                        /*if (LanguageDetector.hasSupport()) {
                             final String[] fromLang = {null};
                             cell.setVisibility(View.GONE);
                             waitForLangDetection.set(true);
@@ -22061,7 +22060,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                                     onLangDetectionDone.getAndSet(null).run();
                                 }
                             }, 250);
-                        } else if (translateButtonEnabled) {
+                        } else */if (translateButtonEnabled) {
                             cell.setOnClickListener(e -> {
                                 if (selectedObject == null || i >= options.size() || getParentActivity() == null) {
                                     return;
