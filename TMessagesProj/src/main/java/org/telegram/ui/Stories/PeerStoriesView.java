144f3f42cc1e6760ceac47d9afc3026be1018688
thermatk <thermatk@thermatk.com>
2022-08-29 00:50:30 +0200

    [TF][KILL] Huawei

--- TMessagesProj/src/main/java/org/telegram/ui/Stories/PeerStoriesView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Stories/PeerStoriesView.java
@@ -2697,7 +2697,6 @@ public class PeerStoriesView extends SizeNotifierFrameLayout implements Notifica
                     LaunchActivity.instance.checkAppUpdate(true);
                 }
             } else if (BuildVars.isHuaweiStoreApp()){
-                Browser.openUrl(getContext(), BuildVars.HUAWEI_STORE_URL);
             } else {
                 Browser.openUrl(getContext(), BuildVars.PLAYSTORE_APP_URL);
             }
