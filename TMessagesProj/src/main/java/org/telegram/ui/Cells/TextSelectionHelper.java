853f81385dc6d16f1f1cb1552d93ad2865dbfb0b
thermatk <thermatk@thermatk.com>
2023-09-17 23:32:26 +0200

    [TF][KILL] Google Translate without MLKit

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/TextSelectionHelper.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/TextSelectionHelper.java
@@ -46,7 +46,6 @@ import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.ApplicationLoader;
 import org.telegram.messenger.Emoji;
 import org.telegram.messenger.FileLog;
-import org.telegram.messenger.LanguageDetector;
 import org.telegram.messenger.LocaleController;
 import org.telegram.messenger.MessageObject;
 import org.telegram.messenger.R;
@@ -1426,7 +1425,7 @@ public abstract class TextSelectionHelper<Cell extends TextSelectionHelper.Selec
                         menu.getItem(2).setVisible(true);
                     }
                 }
-                if (onTranslateListener != null && LanguageDetector.hasSupport() && getSelectedText() != null) {
+                /*if (onTranslateListener != null && LanguageDetector.hasSupport() && getSelectedText() != null) {
                     LanguageDetector.detectLanguage(getSelectedText().toString(), lng -> {
                         translateFromLanguage = lng;
                         updateTranslateButton(menu);
@@ -1436,10 +1435,10 @@ public abstract class TextSelectionHelper<Cell extends TextSelectionHelper.Selec
                         translateFromLanguage = null;
                         updateTranslateButton(menu);
                     });
-                } else {
+                } else {*/
                     translateFromLanguage = null;
                     updateTranslateButton(menu);
-                }
+                //}
                 return true;
             }
 
@@ -1452,7 +1451,7 @@ public abstract class TextSelectionHelper<Cell extends TextSelectionHelper.Selec
                             translateFromLanguage != null &&
                             (!translateFromLanguage.equals(translateToLanguage) || translateFromLanguage.equals("und")) &&
                             !RestrictedLanguagesSelectActivity.getRestrictedLanguages().contains(translateFromLanguage)
-                        ) || !LanguageDetector.hasSupport()
+                        ) //|| !LanguageDetector.hasSupport()
                     )
                 );
             }
--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -152,7 +152,6 @@ import org.telegram.messenger.HashtagSearchController;
 import org.telegram.messenger.ImageLoader;
 import org.telegram.messenger.ImageLocation;
 import org.telegram.messenger.ImageReceiver;
-import org.telegram.messenger.LanguageDetector;
 import org.telegram.messenger.LiteMode;
 import org.telegram.messenger.LocaleController;
 import org.telegram.messenger.MediaController;
@@ -29217,7 +29216,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                                     updateTopPanel(true);
                                 }
                             });
-                        } else if (LanguageDetector.hasSupport()) {
+                        } else /*if (LanguageDetector.hasSupport()) {
                             final String[] fromLang = {null};
                             cell.setVisibility(View.GONE);
                             waitForLangDetection.set(true);
@@ -29268,7 +29267,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                                     onLangDetectionDone.getAndSet(null).run();
                                 }
                             }, 250);
-                        } else if (translateEnabled) {
+                        } else */if (translateEnabled) {
                             cell.setOnClickListener(e -> {
                                 if (selectedObject == null || i >= options.size() || getParentActivity() == null) {
                                     return;
@@ -38899,11 +38898,10 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
 
     private void updateBotHelpCellClick(BotHelpCell cell) {
         final boolean translateButtonEnabled = MessagesController.getInstance(currentAccount).getTranslateController().isContextTranslateEnabled();
-        if (translateButtonEnabled && LanguageDetector.hasSupport()) {
+        if (translateButtonEnabled && false) {
             final CharSequence text = cell.getText();
-            LanguageDetector.detectLanguage(text == null ? "" : text.toString(), lang -> {
+                String lang ="";
                 String toLang = LocaleController.getInstance().getCurrentLocale().getLanguage();
-                if (lang != null && (!lang.equals(toLang) || lang.equals("und")) && !RestrictedLanguagesSelectActivity.getRestrictedLanguages().contains(lang)) {
                     cell.setOnClickListener(e -> {
 
                         ActionBarPopupWindow.ActionBarPopupWindowLayout layout = new ActionBarPopupWindow.ActionBarPopupWindowLayout(getContext());
@@ -38932,12 +38930,6 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                         window.setFocusable(true);
                         window.showAsDropDown(cell, cell.getWidth() / 2 - AndroidUtilities.dp(90), AndroidUtilities.dp(-16), Gravity.BOTTOM | Gravity.LEFT);
                     });
-                } else {
-                    cell.setClickable(false);
-                }
-            }, err -> {
-                cell.setClickable(false);
-            });
         } else {
             cell.setClickable(false);
         }
